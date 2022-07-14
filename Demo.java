2f642e14a679f4826493e10b671ed41f892cb615
Akash <akash300383@gmail.com>
2022-07-14 11:40:06 +0530

    add operation done

--- Demo.java
+++ Demo.java
@@ -1,5 +1,9 @@
 class Demo {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Java");
+		int a=10;
+		int b=20;
+		int sum = a+b;
+		System.out.println("Sum of two number is "+sum);
 	}
 }
